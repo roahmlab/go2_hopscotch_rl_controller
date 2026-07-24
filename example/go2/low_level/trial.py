9eeade1c01e71d15df2b741d28009d92805740ff
vansh <vansht@umich.edu>
2026-07-24 19:00:12 -0400

    new pt

--- example/go2/low_level/trial.py
+++ example/go2/low_level/trial.py
@@ -738,8 +738,8 @@ class Custom:
 if __name__ == '__main__':
     import argparse
     ap = argparse.ArgumentParser()
-    ap.add_argument("--checkpoint", default="hopscotch_utils/model_1499_v18.pt")
-    ap.add_argument("--traj", default="hopscotch_utils/traj_hopscotch_friction.json")
+    ap.add_argument("--checkpoint", default="hopscotch_utils/softarm_1499.pt")
+    ap.add_argument("--traj", default="hopscotch_utils/traj_hopscotch_friction_6cm_lsq.json")
     ap.add_argument("iface", nargs="?", default=None)
     args = ap.parse_args()
 
