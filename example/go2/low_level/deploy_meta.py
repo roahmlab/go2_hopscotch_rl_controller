c37234e54746a7b041ad28013aac0198dcac5f92
vansh <vansht@umich.edu>
2026-08-10 19:33:23 -0400

    kp50 solved contact!

--- example/go2/low_level/deploy_meta.py
+++ example/go2/low_level/deploy_meta.py
@@ -573,6 +573,7 @@ class Custom:
 
         self.KP = float(meta["kp"])
         
+        
         self.KD = float(meta["kd"])
         
         self.ACTION_SCALE = float(meta["action_scale"])
@@ -1503,8 +1504,8 @@ class Custom:
 if __name__ == '__main__':
     import argparse
     ap = argparse.ArgumentParser()
-    ap.add_argument("--checkpoint", default="hopscotch_utils/kp60_1200.pt")
-    ap.add_argument("--meta", default="hopscotch_utils/kp60_meta.json", help="default: meta.json beside the checkpoint")
+    ap.add_argument("--checkpoint", default="hopscotch_utils/model_1200.pt")
+    ap.add_argument("--meta", default="hopscotch_utils/meta.json", help="default: meta.json beside the checkpoint")
     ap.add_argument("--traj", default="traj_hopscotch_friction_6cm_lsq.json", help="override meta's traj_path")
     ap.add_argument("--resid_scale", type=float, default=1,
                     help="residual authority attenuation (1.0 = trained). 0.75 = the "
